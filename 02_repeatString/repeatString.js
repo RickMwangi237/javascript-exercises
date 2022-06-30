bc16c873377da27c24df51a8f3d155e5ff6a505d
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-30 17:21:08 +0300

    Use repeat() to mulitply string value by num

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let repeatedString = string.repeat(num);
+    return repeatedString;
+}; 
+//repeatString("hey",3)
+for (let i =0; i < 20; i++){
+    repeatString("hey",20)
+}
 
 // Do not edit below this line
 module.exports = repeatString;
