b90165561c1ade476a6f2305cd046319c4394aef
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-30 18:04:55 +0300

    Add if statement to repeatString for -ve num value

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,16 @@
-const repeatString = function(string, num) {
-    let repeatedString = string.repeat(num);
-    return repeatedString;
+const repeatString = function(string, num) {    
+    if (num >= 0){
+        let repeatedString = string.repeat(num);
+        return repeatedString;
+    } else {
+        let error = "ERROR";
+        return error;
+    }
 }; 
 //repeatString("hey",3)
-for (let i =0; i < 20; i++){
-    repeatString("hey",20)
+for (let i =0 ; i < 20; i++){
+    repeatString("hey",i)
+    
 }
 
 // Do not edit below this line
