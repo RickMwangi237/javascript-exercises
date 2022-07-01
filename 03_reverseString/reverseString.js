d3283de06e89d11c4342359aa43b5f2c67572fe4
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-01 09:13:19 +0300

    Create function that takes a parameter (string)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+//Create a function that takes a string parameter
+// The argument for this function should declare an array variable whose value is an array of split substrings 
+//Return 
+
+const reverseString = function(string) {
+    //.split() splits strings and creates an array of split substrings
+    // const splitStringArray = string.split("");
+    // //.reverse() reverses the order of an array and returns a reversed array
+    // //.join("") joins the reversed array 
+    // const reversedString = splitStringArray.reverse().join("");
+    // return reversedString;
 
 };
-
-// Do not edit below this line
+// console.log(reverseString("hello there"))
+// console.log(typeof(splitStringArray))
+// // Do not edit below this line
 module.exports = reverseString;
