94f2a3ad5cfe6474dae3b148f229b1c36ce3886d
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-04 19:07:19 +0300

    Create for loop to compare elements in both arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,16 @@
 //Declare a function that takes an array as the first parameter and a rest parameter
-//this rest parameter ...the args will hold all other user input parameters in an array
-const removeFromArray = function(myArray, ...theArgs) {
-    
+//rest parameter ...toRemove holds all other user input parameters in an array
+const removeFromArray = function(myArray, ...toRemove) {
+    //For loop through toRemove[] comparing the value of removables in toRemove to elements of myArray and removing all instances of that removable from myArray
 
+    for(const removable of toRemove){
+        if(myArray.indexOf(removable)!==-1){
+            myArray.splice(myArray.indexOf(removable),1)
+        }
+    }
+    return myArray
 }
+
+removeFromArray([0,1,2,3,4],0,1,2)
 // Do not edit below this line
 module.exports = removeFromArray;
