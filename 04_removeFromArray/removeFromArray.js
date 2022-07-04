4125ad3707b5985edd28dd3e3730c99bf3573e12
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-04 18:51:32 +0300

    Declare a function with array and rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
+//Declare a function that takes an array as the first parameter and a rest parameter
+//this rest parameter ...the args will hold all other user input parameters in an array
+const removeFromArray = function(myArray, ...theArgs) {
+    
 
-};
-
+}
 // Do not edit below this line
 module.exports = removeFromArray;
