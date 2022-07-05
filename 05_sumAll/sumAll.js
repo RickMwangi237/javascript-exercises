0db788d4511d4d457e22d7dd2a9ec6826753ee7b
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-05 08:41:48 +0300

    Add loop that adds value of toSum[] items to sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -10,12 +10,14 @@ const sumAll = function(param1, param2) {
           } else if((typeof(param1) != "number")||(typeof(param2) != "number")){
               return "ERROR";
          } else if (param1 > param2) {
+             //loop that appends items into the empty array including the values of param1 and param2
               for(let i = param1; i>=param2; i--){
                  toSum.push(i);
                 };
-    //          for (let i = 0; i < toSum.length; i++) {
-    //             sum += toSum[i];
-    //          };
+                //loop that takes array items and adds their value to that of the sum variable
+              for (let i = 0; i < toSum.length; i++) {
+                sum += toSum[i];
+              };
     //          return (sum);
     //     } else if(param1 < param2){
     //         for(let i = param1; i<=param2; i++){
