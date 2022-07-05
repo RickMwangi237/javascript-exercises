cadbb757a855a91c1b1049dd2c64b75f8d7297c2
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-05 08:47:27 +0300

    Add loop to add array items and sum their value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -18,16 +18,17 @@ const sumAll = function(param1, param2) {
               for (let i = 0; i < toSum.length; i++) {
                 sum += toSum[i];
               };
-    //          return (sum);
-    //     } else if(param1 < param2){
-    //         for(let i = param1; i<=param2; i++){
-    //             toSum.push(i);
-    //         }
-    //         for (let i = 0; i < toSum.length; i++) {
-    //              sum += toSum[i];
-    //         };
-    //         return sum;
-    //     }
+             return (sum);
+        } else if(param1 < param2){
+            //These for loops function in the same way as the ones directly above
+            for(let i = param1; i<=param2; i++){
+                toSum.push(i);
+            }
+            for (let i = 0; i < toSum.length; i++) {
+                 sum += toSum[i];
+            };
+            return sum;
+        }
       
     }
     
