f9c04839a18456c392954cdf9d03b704fd8b566d
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-05 08:35:23 +0300

    Add if statement with conditions for tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,15 +4,15 @@ const sumAll = function(param1, param2) {
         let toSum = [];
         let sum = 0;
         
-
-    //      if ((param1 < 0) || (param2 < 0)){
-    //          return "ERROR";
-    //      } else if((typeof(param1) != "number")||(typeof(param2) != "number")){
-    //          return "ERROR";
-    //     } else if (param1 > param2) {
-    //          for(let i = param1; i>=param2; i--){
-    //             toSum.push(i);
-    //             };
+        //if statement with conditions for all tests in .spec.js
+         if ((param1 < 0) || (param2 < 0)){
+              return "ERROR";
+          } else if((typeof(param1) != "number")||(typeof(param2) != "number")){
+              return "ERROR";
+         } else if (param1 > param2) {
+              for(let i = param1; i>=param2; i--){
+                 toSum.push(i);
+                };
     //          for (let i = 0; i < toSum.length; i++) {
     //             sum += toSum[i];
     //          };
