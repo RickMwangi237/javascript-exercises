5d687db811a219ce266341dbcc414f4d5c22b622
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-05 09:31:49 +0300

    Add if statement with all conditions for leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
 //Declare function that takes a year parameter
 const leapYears = function(year) {
-
+    //Add if statement with all conditions necessary for leap years and return true if they are all met
+    if ((year % 4 === 0) && (year % 400 === 0)){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
