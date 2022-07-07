28506373a435ebd33ad322e6bd22cd11bddfd84e
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-07 08:21:59 +0300

    Declare a function that takes parameter farenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,15 @@
-const ftoc = function() {
+// Declare a function that with parameter farenheit
+const ftoc = function(f) {
+  // let degCelcius = (f-32) * 5/ 9;
+  // console.log(degCelcius);
 
+  // if (degCelcius - Math.floor(degCelcius) !== 0){
+  //   return  Number(degCelcius.toFixed(1));
+  // } else {return degCelcius};
+
+  
 };
+ftoc(-100)
 
 const ctof = function() {
 
