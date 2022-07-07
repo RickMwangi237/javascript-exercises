6b335843b34ac16293ef3bbb2066eb95d1826689
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-07 11:27:46 +0300

    Declare a funtion that takes a string parameter

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+//Declare a funtion that takes a string parameter
+const palindromes = function (string) {
+    //Use .split to create an array of split sub string elements
+    let splitStringArray = string.split("");
 
-};
+//     //Use for loop to remove blank space from array
+//     for (let element of splitStringArray){
+//         if (element === " "){
+//             splitStringArray.splice(splitStringArray.indexOf(element),1)
+//         }
+//     }  
 
-// Do not edit below this line
-module.exports = palindromes;
+//  console.log(splitStringArray);
+
+//     const splitString = splitStringArray.join("");
+//     const reverseString = splitStringArray.reverse().join("");
+
+//     if (splitString === reverseString){
+//         console.log(true);
+//         return false;
+//     } else {
+//         console.log(false);
+//         return false;
+//     }
+//  };
+// palindromes("racecar")
+// // Do not edit below this line
+// module.exports = palindromes;
