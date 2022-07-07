067c9eb3a989dc9e3791c64c572a232ec9dfe8fb
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-07 11:30:06 +0300

    Use .split to create array of split sub string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,12 +3,12 @@ const palindromes = function (string) {
     //Use .split to create an array of split sub string elements
     let splitStringArray = string.split("");
 
-//     //Use for loop to remove blank space from array
-//     for (let element of splitStringArray){
-//         if (element === " "){
-//             splitStringArray.splice(splitStringArray.indexOf(element),1)
-//         }
-//     }  
+     //Use for loop to remove blank space from array
+    // for (let element of splitStringArray){
+    //     if (element === " "){
+    //         splitStringArray.splice(splitStringArray.indexOf(element),1)
+    //     }
+    // }  
 
 //  console.log(splitStringArray);
 
@@ -22,7 +22,7 @@ const palindromes = function (string) {
 //         console.log(false);
 //         return false;
 //     }
-//  };
+  };
 // palindromes("racecar")
 // // Do not edit below this line
 // module.exports = palindromes;
