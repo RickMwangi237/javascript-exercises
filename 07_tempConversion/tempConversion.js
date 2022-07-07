e617e68ae63c3b46706cb03ddb32ea212d8e0e41
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-07 08:28:51 +0300

    Add if statement to check if whole or fraction

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
-// Declare a function that with parameter farenheit
+// Declare a function with parameter f for farenheit
 const ftoc = function(f) {
   // Declare a variable that with formula for farenheit to celcius as it's value 
   let degCelcius = (f-32) * 5/ 9;
-  // console.log(degCelcius);
+  console.log(degCelcius);
 
-  // if (degCelcius - Math.floor(degCelcius) !== 0){
-  //   return  Number(degCelcius.toFixed(1));
-  // } else {return degCelcius};
+  // Add if statement to check if variable is whole number or fraction
+  if (degCelcius - Math.floor(degCelcius) !== 0){
+    return  Number(degCelcius.toFixed(1));
+  } else {return degCelcius};
 
-  
 };
 ftoc(-100)
 
