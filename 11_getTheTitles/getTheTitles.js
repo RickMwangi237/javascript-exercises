2457bcd173fb69db944e1f4a2ebd1bbd4b4fe1f4
Rick Mwangi <rickmwangi237@protonmail.com>
2022-07-12 12:19:26 +0300

    Add array for testing purposes

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function(books) {
-     const titles = [];
-};
+// Declare a books array for test purposes
+const books = [
+   {
+     title: 'Book',
+     author: 'Name'
+   },
+   {
+     title: 'Book2',
+     author: 'Name2'
+   }
+ ];
+
+// const getTheTitles = function(books) {
+//      const titlesArray = [];
+//      const allBooks = {};
+
+
+//      for (let book of books){
+//         titlesArray.push(book.title)
+//      }
+//      console.log(titlesArray)
+// };
+
+//  getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
